18315a002476c1da330045ca3b25217d5d901ecf
Harlan Heath <HarlanDHeath@gmail.com>
2018-07-24 10:28:48 -0500

    server update

---
+++ server/Controllers/getProducts.js
@@ -0,0 +1,12 @@
+module.exports = {
+  getProducts: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    dbInstance
+      .all_abrush()
+      .then(products => res.status(200).send(products))
+      .catch(err => {
+        res.status(500).send(err);
+        console.log("Something has gone wrong!");
+      });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -3,10 +3,19 @@ const express = require("express");
 const { json } = require("body-parser");
 const session = require("express-session");
 const massive = require("massive");
+const controller = require("./Controllers/getProducts");
+const axios = require("axios");
 
 const port = 3001;
 const app = express();
+
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance);
+});
+
 app.use(json());
+
+app.get("/api/products", controller.getProducts);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
