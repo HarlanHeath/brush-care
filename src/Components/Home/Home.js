253eb36d94f11029358580a29de4186d9438ae9b
Harlan Heath <HarlanDHeath@gmail.com>
2018-07-24 15:03:21 -0500

    home styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,12 @@ import "./Home.css";
 
 export default class Home extends Component {
   render() {
-    return <div className="HomePage">I'm a Homepage!</div>;
+    return (
+      <div className="HomePage">
+        <h2 className="Slogan">
+          The world’s finest art materials, for the world’s finest artists.
+        </h2>
+      </div>
+    );
   }
 }
