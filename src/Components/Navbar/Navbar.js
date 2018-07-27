1d50b9abed23cccf919cf7908034e45ad1853ae5
Harlan Heath <HarlanDHeath@gmail.com>
2018-07-27 16:54:12 -0500

    end of day update

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,18 +6,23 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="Mainbar">
-        <Link to="/" className="route">
-          Home
-        </Link>
-        <Link to="/products" className="route">
-          Products
-        </Link>
-        <Link to="/cart" className="route">
-          Cart
-        </Link>
-        <Link to="/login" className="route">
-          Login
-        </Link>
+        <div class="dropdown">
+          <button class="dropbtn">Dropdown</button>
+          <div class="dropdown-content">
+            <Link to="/" className="route">
+              Home
+            </Link>
+            <Link to="/products" className="route">
+              Products
+            </Link>
+            <Link to="/cart" className="route">
+              Cart
+            </Link>
+            <Link to="/login" className="route">
+              Login
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
