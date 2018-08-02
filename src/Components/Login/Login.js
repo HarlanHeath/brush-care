2b422fd581d6085cceaad2bd404005740f49236a
Harlan Heath <HarlanDHeath@gmail.com>
2018-08-02 12:27:53 -0500

    css update and pre-stripe

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,7 +4,7 @@ import "./Login.css";
 class Login extends Component {
   render() {
     return (
-      <div className="logPage">
+      <div className="logpage">
         <div className="logbuttons">
           <a href={process.env.REACT_APP_LOGIN}>
             <button>Login</button>
