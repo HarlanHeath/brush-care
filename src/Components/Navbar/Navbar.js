7c7d1124808f2ba1fe3d7a53b10ac3d96d458c6a
Harlan Heath <HarlanDHeath@gmail.com>
2018-08-08 11:07:48 -0500

    setup login and logout condition

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Login from "../Login/Login";
+import axios from "axios";
+// import Login from "../Login/Login";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      logged: false
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`/api/me`)
+      .then(res1 => {
+        console.log(res1);
+        this.setState({
+          logged: true
+        });
+      })
+      .catch(console.log);
+  }
+
   render() {
     return (
       <div className="Mainbar">
+        {/* {console.log(this.state.logged)} */}
         <div className="full-menu">
           <Link to="/" className="route">
             Home
@@ -17,12 +38,21 @@ export default class Navbar extends Component {
           <Link to="/cart" className="route">
             Cart
           </Link>
-          <a href={process.env.REACT_APP_LOGIN} className="route">
+          {this.state.logged ? (
+            <a href={process.env.REACT_APP_LOGOUT} className="route">
+              Logout
+            </a>
+          ) : (
+            <a href={process.env.REACT_APP_LOGIN} className="route">
+              Login
+            </a>
+          )}
+          {/* <a href={process.env.REACT_APP_LOGIN} className="route">
             Login
           </a>
           <a href={process.env.REACT_APP_LOGOUT} className="route">
             Logout
-          </a>
+          </a> */}
         </div>
         <div className="dropdown">
           <button className="dropbtn">Menu</button>
@@ -36,12 +66,15 @@ export default class Navbar extends Component {
             <Link to="/cart" className="route">
               Cart
             </Link>
-            <a href={process.env.REACT_APP_LOGIN} className="route">
-              Login
-            </a>
-            <a href={process.env.REACT_APP_LOGOUT} className="route">
-              Logout
-            </a>
+            {this.state.logged ? (
+              <a href={process.env.REACT_APP_LOGOUT} className="route">
+                Logout
+              </a>
+            ) : (
+              <a href={process.env.REACT_APP_LOGIN} className="route">
+                Login
+              </a>
+            )}
           </div>
         </div>
       </div>
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,11 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Products from "./Components/Products/Products";
 import Cart from "./Components/Cart/Cart";
-import Login from "./Components/Login/Login";
+// import Login from "./Components/Login/Login";
 
 export default (
   <Switch>
-    <Route path="/login" component={Login} />
+    {/* <Route path="/login" component={Login} /> */}
     <Route path="/products" component={Products} />
     <Route path="/cart" component={Cart} />
     <Route exact path="/" component={Home} />
